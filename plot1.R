ca05254b9a3af2dcff1b87df143aacd1d9f01016
ifmoreno <if.moreno905@uniandes.edu.co>
2021-06-14 20:50:34 -0500

    plot1

---
+++ plot1.R
@@ -0,0 +1,25 @@
+
+
+url <- "https://d396qusza40orc.cloudatosront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+camino <- getwd()
+download.file(url, file.path(camino, "datos.zip"))
+unzip(zipfile = "datos.zip")
+
+# actividades y variables
+list.files()[4]
+
+datos <- read.table(file="household_power_consumption.txt", sep = ";", header = T, na.strings = "?")
+datos$Date <- as.Date(datos$Date, "%d/%m/%Y")
+
+datos <- datos[datos$Date >= "2007-02-01" & datos$Date <= "2007-02-02",]
+datos$tiempo <- paste0(datos$Date," ", datos$Time)
+datos$tiempo <- strptime(datos$tiempo, format = "%Y-%m-%d %H:%M:%S")
+datos$Global_active_power <- as.numeric(datos$Global_active_power)
+
+png("plot1.png")
+hist(datos$Global_active_power, 
+     main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)", 
+     ylab = "Frequency", 
+     col = "red")
+dev.off()
